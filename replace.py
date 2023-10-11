08a4ef74b5377668f88cf307d93c4fc7ab34bece
Pedro Altimari <83617644+fontespedro@users.noreply.github.com>
2023-10-11 07:33:27 -0300

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,3 +1,11 @@
+# G R A F A N A
+
+# Script que utilizei para substituir o nome de um host para outro host do mesmo host group com os mesmos itens utilizados.
+# No caso exportei o JSON Model do Grafana, colei em um .txt e executei o script com nome do host antigo para o nome do host novo.
+# Depois importei o arquivo no Grafana e funcionou perfeitamente. :)
+
+# Obs: isso pode ser feito de item to item, item tag to item tag, host group to host group etc.
+
 nome_arquivo_entrada = 'test.txt' # Arquivo
 nome_arquivo_saida = 'new-test.txt' # Arquivo que será criado
 palavra_antiga = 'RA-SW-L2-03' # Palavra que será removida
